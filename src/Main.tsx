ca61c5e8b928544ec696fdfa3f832b3068612435
gness1804 <graham.nessler@gmail.com>
2021-02-25 22:15:03 -0600

    got rough mvp of game done

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,14 +1,22 @@
 import React, { useEffect, useState } from 'react';
-import WordLine from './WordLine';
 import axios from 'axios';
+import { v4 } from 'uuid';
+import WordLine from './WordLine';
 
 const Main = () => {
   const [word, setWord] = useState('');
   const [loading, setLoading] = useState(false);
+  const [letter, setLetter] = useState('');
+  const [successfulLetters, setSuccessfulLetters] = useState<string[]>([]);
+  const [failedLetters, setFailedLetters] = useState<string[]>([]);
 
   useEffect(() => {
     getWord();
   }, []);
+
+  useEffect(() => {
+    checkGameStatus();
+  });
 
   const getWord = async (): Promise<string | undefined> => {
     try {
@@ -26,9 +34,47 @@ const Main = () => {
     }
   };
 
+  const verifyLetter = (): void => {
+    if (word.includes(letter)) setSuccessfulLetters((arr) => [...arr, letter]);
+    else setFailedLetters((arr) => [...arr, letter]);
+    setLetter('');
+  };
+
+  const checkGameStatus = (): void => {
+    if (loading) return;
+    if (failedLetters.length > 6) alert('You lose!');
+    if (
+      successfulLetters.length > 0 &&
+      successfulLetters.length === word.length
+    )
+      alert('You win!');
+  };
+
   return (
     <div>
-      {loading ? <p>Retrieving your word...</p> : <WordLine word={word} />}
+      {loading ? (
+        <p>Retrieving your word...</p>
+      ) : (
+        <WordLine word={word} successfulLetters={successfulLetters} />
+      )}
+      {!loading && (
+        <div>
+          <label htmlFor="letterInput">Enter a letter:</label>
+          <input
+            id="letterInput"
+            type="text"
+            size={1}
+            minLength={1}
+            maxLength={1}
+            onChange={(e) => setLetter(e.target.value)}
+            value={letter}
+          />
+          <button onClick={verifyLetter}>Verify Letter</button>
+          {failedLetters.map((_letter) => (
+            <div key={v4()}>{_letter}</div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
--- src/WordLine.tsx
+++ src/WordLine.tsx
@@ -3,14 +3,19 @@ import { v4 } from 'uuid';
 
 interface Props {
   word: string;
+  successfulLetters: string[];
 }
 
-const WordLine = ({ word }: Props) => {
+const WordLine = ({ word, successfulLetters }: Props) => {
   return (
     <div>
-      {word.split('').map((letter) => (
-        <div key={v4()}>{letter}</div>
-      ))}
+      {word.split('').map((letter) => {
+        if (successfulLetters.includes(letter)) {
+          return <div key={v4()}>{letter}</div>;
+        } else {
+          return <div key={v4()}>__</div>;
+        }
+      })}
     </div>
   );
 };
