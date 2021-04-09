0818331e4146f4ddd19d315dcd89b07e7ef6b4f1
gness1804 <graham.nessler@gmail.com>
2021-04-08 19:48:34 -0500

    add some basic styling for Defeat page and redirect Defeat page back home if no word

--- src/Defeat/index.tsx
+++ src/Defeat/index.tsx
@@ -1,14 +1,32 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 
 interface Props {
   word: string;
 }
 
 const Defeat = ({ word }: Props) => {
+  const history = useHistory();
+
+  useEffect(() => {
+    if (!word) {
+      setTimeout(() => {
+        history.push('/');
+      }, 3000);
+    }
+  }, []);
+
+  if (!word) {
+    return (
+      <div className="defeat-no-word">
+        <h2>No word! Redirecting back home.</h2>
+      </div>
+    );
+  }
+
   return (
-    <div>
-      <h2>You Lose!</h2>
+    <div className="defeat dark">
+      <h2 className="defeat-heading">You Lose!</h2>
       <p>
         The word was: <em>{word}</em>.
       </p>
