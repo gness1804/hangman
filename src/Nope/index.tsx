f5a40ba7c22b49f304e9ad99b28454cc976628de
gness1804 <graham.nessler@gmail.com>
2021-05-10 19:45:38 -0500

    style 404 page

--- src/Nope/index.tsx
+++ src/Nope/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { useLocation, Link } from 'react-router-dom';
+import './index.css';
 
 const Nope = () => {
   const location = useLocation();
 
   return (
-    <div>
-      <h2>Page Not Found!</h2>
+    <div className="nope dark">
+      <h2 className="nope-heading">Page Not Found!</h2>
       <p>
         Oops! We could not find the page at <code>{location.pathname}</code>.
         Please try again.
